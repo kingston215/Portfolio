e5ef261575ed87d2cb62d4b16c183c85eb158123
Kenneth Smith <smithkenneth15@yahoo.com>
2024-06-23 17:16:15 -0400

    no message

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new yped(".text", {
+    strings: [" Full Stack Developer ", "Cyber Security Engineer", "Web Developer", "DevSecOps Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
